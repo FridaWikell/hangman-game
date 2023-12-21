4e794c0eb382a07438ca06e21782cb4f9fa3348d
FridaWikell <>
2023-12-21 13:06:21 +0000

    Add: Declare variabels

---
+++ assets/script/script.js
@@ -0,0 +1,12 @@
+let nintendoCharacters = [
+    "toadstool",
+    "toad",
+    "link",
+    "zelda"
+]
+
+let answer = '';
+let allowedMistakes = 7;
+let mistakesDone = 0;
+let guesses = [];
+let status = null;
